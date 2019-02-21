f5e6213327f7e731afb64aca453ba96565cc08a3
carlocadiz <carlocadiz71@gmail.com>
2019-02-21 20:03:48 +0000

    setup files and created first two unit tests

---
+++ nameFormatter.js
@@ -0,0 +1,13 @@
+class NameFormatter {
+
+  invertName(name) {
+    if (name === '') {
+      return '';
+    }
+
+    return name;
+  }
+
+}
+
+module.exports = NameFormatter;
---
+++ readme.md
@@ -0,0 +1,2 @@
+Unit testing example where the unit test are writting in conjunction with production code.
+Use mocha chai libraries. Can run test using 'npm test'
